b18539015f6e834360a992b621bd5b0648faedf5
waqas10075 <waqas10075@gmail.com>
2020-11-20 00:31:08 +0500

    merge

--- src/components/molecules/wallet/WalletData.tsx
+++ src/components/molecules/wallet/WalletData.tsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../store/reducers/Index";
 
 function WalletData() {
+  const { balances } = useSelector((state: RootState) => state.wallet);
   return (
     <div className="flex mt-8 px-24 mb-16">
       <div className="w-full bg-customGray-100 rounded">
@@ -9,19 +12,66 @@ function WalletData() {
         </div>
         <table width="100%">
           <tr className="bg-gray-300 text-xxs text-left text-gray-600 font-medium">
-            <td className="w-1/5 pl-2 flex items-center my-2">ASSETS <img src="assets/Icons/up-down-arrow.svg" className="ml-1 h-2" /></td>
-            <td className="w-1/5"><div className="flex items-center">DESCRIPTION <img src="assets/Icons/up-down-arrow.svg" className="ml-1 h-2" /></div></td>
-            <td className="w-1/5"><div className="flex items-center">Total <img src="assets/Icons/up-down-arrow.svg" className="ml-1 h-2" /></div></td>
-            <td className="w-1/5"><div className="flex items-center">USD Value <img src="assets/Icons/up-down-arrow.svg" className="ml-1 h-2" /></div></td>
-            <td className="w-1/5"><div className="flex items-center">Action</div></td>
+            <td className="w-1/5 pl-2 flex items-center my-2">
+              ASSETS{" "}
+              <img src="assets/Icons/up-down-arrow.svg" className="ml-1 h-2" />
+            </td>
+            <td className="w-1/5">
+              <div className="flex items-center">
+                DESCRIPTION{" "}
+                <img
+                  src="assets/Icons/up-down-arrow.svg"
+                  className="ml-1 h-2"
+                />
+              </div>
+            </td>
+            <td className="w-1/5">
+              <div className="flex items-center">
+                Total{" "}
+                <img
+                  src="assets/Icons/up-down-arrow.svg"
+                  className="ml-1 h-2"
+                />
+              </div>
+            </td>
+            <td className="w-1/5">
+              <div className="flex items-center">
+                USD Value{" "}
+                <img
+                  src="assets/Icons/up-down-arrow.svg"
+                  className="ml-1 h-2"
+                />
+              </div>
+            </td>
           </tr>
-          <tbody></tbody>
+          <tbody>
+            {balances.map((item) => (
+              <tr className="py-20 text-xxs text-left text-gray-600 font-medium">
+                <td className="w-1/5 pl-2 flex items-center my-2">
+                  {item.short}
+                </td>
+                <td className="w-1/5">{item.name}</td>
+                <td className="w-1/5">
+                  <div className="flex items-center">
+                    {item.cryptoBalance} {item.short}
+                  </div>
+                </td>
+                <td className="w-1/5">
+                  <div className="flex items-center">
+                    ${item.cryptoBalance * item.rate}
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
         </table>
+        {balances.length <= 0 && (
         <div className="py-20 flex justify-center items-center">
           <h6 className="text-gray-600 font-medium text-xxs">
             No assets associated with this wallet
           </h6>
         </div>
+        )}
       </div>
       {/* <div className="w-84 bg-customGray-100 rounded">
         <div className="bg-gray-300 rounded-t pl-2 pt-2">
