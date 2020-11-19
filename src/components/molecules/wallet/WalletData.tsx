941e1145cad5f6b59140393e5563ec8fe76d2d34
muneeburrehman.codingpixel@gmail.com <muneeburrehman.codingpixel@gmail.com>
2020-11-19 18:58:30 +0500

    market redesigned

--- src/components/molecules/wallet/WalletData.tsx
+++ src/components/molecules/wallet/WalletData.tsx
@@ -3,13 +3,13 @@ import React from "react";
 function WalletData() {
   return (
     <div className="flex mt-8 px-24 mb-16">
-      <div className="w-full bg-customGray-100 rounded mr-8">
+      <div className="w-full bg-customGray-100 rounded">
         <div className="bg-gray-300 rounded-t pl-2 pt-2">
           <h3 className="font-medium text-xs text-gray-600">YOUR SYNTHS</h3>
         </div>
         <table width="100%">
           <tr className="bg-gray-300 text-xxs text-left text-gray-600 font-medium">
-            <td className="w-1/5 pl-2 flex items-center">ASSETS <img src="assets/Icons/up-down-arrow.svg" className="ml-1 h-2" /></td>
+            <td className="w-1/5 pl-2 flex items-center my-2">ASSETS <img src="assets/Icons/up-down-arrow.svg" className="ml-1 h-2" /></td>
             <td className="w-1/5"><div className="flex items-center">DESCRIPTION <img src="assets/Icons/up-down-arrow.svg" className="ml-1 h-2" /></div></td>
             <td className="w-1/5"><div className="flex items-center">Total <img src="assets/Icons/up-down-arrow.svg" className="ml-1 h-2" /></div></td>
             <td className="w-1/5"><div className="flex items-center">USD Value <img src="assets/Icons/up-down-arrow.svg" className="ml-1 h-2" /></div></td>
@@ -23,7 +23,7 @@ function WalletData() {
           </h6>
         </div>
       </div>
-      <div className="w-84 bg-customGray-100 rounded">
+      {/* <div className="w-84 bg-customGray-100 rounded">
         <div className="bg-gray-300 rounded-t pl-2 pt-2">
           <h3 className="font-medium text-xs text-gray-600">ASSETS</h3>
         </div>
@@ -35,7 +35,7 @@ function WalletData() {
           </tr>
           <tbody></tbody>
         </table>
-      </div>
+      </div> */}
     </div>
   );
 }
