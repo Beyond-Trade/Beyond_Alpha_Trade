4fb7f9a6a472a8f04e6b4570f5d47bdb0288d6cb
waqas10075 <waqas10075@gmail.com>
2020-11-20 15:28:48 +0500

    mint

--- src/services/swap.service.ts
+++ src/services/swap.service.ts
@@ -1,39 +1,31 @@
 import Web3 from 'web3';
-
-
-import { ERC20Contracts } from "../contracts/constants/contracts";
 import { store } from '../App';
-import { saveBalanceInfoAction } from '../store/actions/WalletActions';
-import { Balance } from '../store/types/WalletState';
+import { ERC20Contracts } from "../contracts/constants/contracts";
 import { ContractLookup } from '../contracts/contracts.lookup';
-import { SyntheticCategories } from '../contracts/constants/synthetic.enum';
-
 let web3: Web3 = new Web3();
 
+export const buyBYNToken = async (amount?: string) => {
+    web3 = store.getState().wallet.web3;
+    let walletInfo = store.getState().wallet;
 
-
-
-export const buyBYNToken = async (address:string, amount?: string) => {
+    let activeAddress = walletInfo.selected.address;
     // @ts-ignore
+
     amount = web3.utils.toWei(amount, 'ether');
 
     if (web3.currentProvider) {
         const contractInfo = ContractLookup.find(contract => contract.contractName === ERC20Contracts.BEYOND)
         if (contractInfo) {
-            const contract = new web3.eth.Contract(contractInfo.contractAbi, contractInfo?.contractAddress, { from:address });
-            try {
-                const tx = await web3.eth.sendTransaction({
-                    from: address,
-                    to: contractInfo.contractAddress,
-                    value: amount as unknown as string,
-                    data: contract.methods.buyTokens().encodeABI()
-                })
-                return tx;
-            } catch (error) {
-                return error;
-            }
-        }
+            const contract = new web3.eth.Contract(contractInfo.contractAbi, contractInfo?.contractAddress, { from: activeAddress });
 
+            const tx = await web3.eth.sendTransaction({
+                from: activeAddress,
+                to: contractInfo.contractAddress,
+                value: amount as unknown as string,
+                data: contract.methods.buyTokens().encodeABI()
+            })
+            return tx;
+        }
     }
     else
         return null;
